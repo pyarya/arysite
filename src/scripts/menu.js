3c5dda8025af2d553b47dd219daac6d6395df5bd
pyarya <aryan.aryal86@gmail.com>
2024-03-23 23:33:39 -0600

    icon + tags

--- src/scripts/menu.js
+++
@@ -1,6 +0,0 @@
-// burger menu interaction
-
-document.querySelector('.burgermenu')?.addEventListener('click', () => {
-    document.querySelector('.nav-links , .alt-links')?.classList.toggle('expanded');
-});
-
