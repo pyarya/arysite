e1b51c62bfd94a444279113270452550f45b2d59
a-a <37915341+pyarya@users.noreply.github.com>
2024-07-27 01:45:56 -0600

    lord help me

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,49 +1,25 @@
-import { useState } from 'preact/hooks';
+import { useEffect, useState } from 'preact/hooks';
+// Adjust the path as necessary
 import '../styles/gallery.css';
 
-const images = [
-    { src: '../../images/art/IMG_4522.DNG', description: 'Description for Image 1' },
-    { src: '../../images/art/IMG_4521.DNG', description: 'Description for Image 2' },
-    { src: '../../images/art/IMG_4548.jpg', description: 'Description for Image 1' },
-    { src: '../../images/art/IMG_4551.jpg', description: 'Description for Image 2' },
-
-  // Add more images as needed
-];
-
-const ImageGallery = () => {
-  const [selectedImage, setSelectedImage] = useState(null);
-
-  const handleImageClick = (image) => {
-    setSelectedImage(image);
-  };
-
-  const handleClosePopup = () => {
-    e.stopPropagation();
-    setSelectedImage(null);
-  };
+const ImageGrid = ({ images }) => {
+  const imageClasses = [
+    'div1', 'div2', 'div3', 'div4', 'div5',
+    'div6', 'div7', 'div8', 'div9', 'div10',
+    'div11', 'div12',  'div13', 'div14', 'div15',
+    'div16', 'div17', 'div18', 'div19', 'div20',
+    'div21', 'div22', 'div23', 'div24',
+  ];
 
   return (
-    <div className="image-gallery">
-      {images.map((image, index) => (
-        <div key={index} className="image-container">
-          <img
-            src={image.src}
-            alt={`Image ${index + 1}`}
-            onClick={() => handleImageClick(image)}
-          />
+    <div className="parent">
+      {images.slice(0, 24).map((image, index) => (
+        <div className={imageClasses[index]} key={index}>
+          <img src={image.src} alt={image.alt} />
         </div>
       ))}
-
-      {selectedImage && (
-        <div className="popup" onClick={() => setSelectedImage(null)}>
-          <div className="popup-content" onClick={handleClosePopup}>
-            <img src={selectedImage.src} alt="Selected" />
-            <p>{selectedImage.description}</p>
-          </div>
-        </div>
-      )}
     </div>
   );
 };
 
-export default ImageGallery;
+export default ImageGrid;
