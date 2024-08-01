a2e27dac35b86993ee49cf37f9ee8e2838e28bbd
a-a <37915341+pyarya@users.noreply.github.com>
2024-08-01 00:02:44 -0600

    add clicking another dday meow!!!!

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 import html from "remark-html";
-
 import cloudflare from "@astrojs/cloudflare";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://pyarya.pages.dev',
-  integrations: [preact()],
+  integrations: [preact(), react()],
   markdown: {
     extendDefaultPlugins: true,
     remarkPlugins: [html],
