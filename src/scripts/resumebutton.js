10b04754116236ebc4825f93dd98a0d05d20bdba
pyarya <aryan.aryal86@gmail.com>
2024-10-11 16:07:32 -0600

    fixed resume stuff

--- src/scripts/resumebutton.js
+++ src/scripts/resumebutton.js
@@ -1,7 +1,19 @@
-document.getElementsByClassName('resume')[0].addEventListener('click', async () => {
-    const url = "https://compile-overleaf.vercel.app/api/read?token=jtfnyyspfxxz";
-    const response = await fetch(url);
-    const jsonResponse = await response.json();
-    const pdfLink = jsonResponse.link.pdf;
-    window.open(pdfLink, "_blank");
-});
+const resumebuttons = document.getElementsByClassName('resume');
+
+for (let i = 0; i < resumebuttons.length; i++) {
+    resumebuttons[i].addEventListener('click', async () => {
+        const url = "https://compile-overleaf.vercel.app/api/read?token=jtfnyyspfxxz";
+        try {
+            const response = await fetch(url);
+            if (!response.ok) {
+                throw new Error('fetch fail');
+            }
+            const jsonResponse = await response.json();
+            const pdfLink = jsonResponse.link.pdf;
+            window.open(pdfLink, "_blank");
+        } catch (error) {
+            console.error('could not fetch pdf from overleaf, opening local', error);
+            window.open('/public/Aryan Aryal Resume.pdf', "_blank"); 
+        }
+    });
+}
