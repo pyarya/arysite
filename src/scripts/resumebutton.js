cf9f620b43525b293a495d4cc6e15a26285d46c0
pyarya <aryan.aryal86@gmail.com>
2024-11-29 12:47:41 -0700

    better script placement and loads link on page load

--- src/scripts/resumebutton.js
+++ src/scripts/resumebutton.js
@@ -1,20 +1,38 @@
 // This script uses https://github.com/NeverBehave/compile-overleaf to get the updated pdf from overleaf and open it in a new tab.
+let preloadedPdfLink = null;
+
+window.addEventListener('DOMContentLoaded', async () => {
+    const url = "https://compile-overleaf.vercel.app/api/read?token=jtfnyyspfxxz";
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const jsonResponse = await response.json();
+            preloadedPdfLink = jsonResponse.link.pdf;
+        } else {
+            console.error('prefetch fail');
+        }
+    } catch (error) {
+        console.error('Error prefetching link:', error);
+    }
+});
 
 document.addEventListener('click', async (event) => {
     if (event.target.classList.contains('resume')) {
-        const url = "https://compile-overleaf.vercel.app/api/read?token=jtfnyyspfxxz";
         console.log('clicked button');
-        try {
-            const response = await fetch(url);
-            if (!response.ok) {
-                throw new Error('pdf fetch fail');
+        if (preloadedPdfLink) {
+            window.open(preloadedPdfLink);
+        } else {
+            try {
+                const response = await fetch(url);
+                if (!response.ok) {
+                    throw new Error('PDF fetch failed');
+                }
+                const jsonResponse = await response.json();
+                window.open(jsonResponse.link.pdf);
+            } catch (error) {
+                console.error('overleaf fetch failed, opening local:', error);
+                window.open('/public/Aryan Aryal Resume.pdf', "_blank");
             }
-            const jsonResponse = await response.json();
-            const pdfLink = jsonResponse.link.pdf;
-            window.open(pdfLink);
-        } catch (error) {
-            console.error('could not fetch pdf from overleaf, opening local', error);
-            window.open('/public/Aryan Aryal Resume.pdf', "_blank"); 
         }
     }
 });
